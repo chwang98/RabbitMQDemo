86602fc76f7bf3d70f7afdd98fe07614fac7870e
wangjunjie <chwang98@qq.com>
2020-07-31 23:34:05 +0800

    Initial commit

---
+++ src/main/java/cn/codewjj/rabbitmq/MainClass.java
@@ -0,0 +1,16 @@
+package cn.codewjj.rabbitmq;
+
+/**
+ * @author codewjj
+ * @version 1.0.0
+ * @ClassName MainClass.java
+ * @Description TODO
+ * @createTime 2020年07月31日 23:33:00
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+
+
+    }
+}
